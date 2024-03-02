11c0357f98ccf9a2b01f3a5e2949a1bc2af9b36a
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2024-03-02 17:59:26 -0400

    verificando se eh primo

--- seguranca_de_computadores/RSA.c
+++ seguranca_de_computadores/RSA.c
@@ -6,12 +6,24 @@ int achar_n(int p, int q){
     return p*q;
 }
 
+//verificar num primo para o totienti
+bool eh_primo(int num) {
+    if (num <= 1)
+        return false;
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+
+
 int main (){
     int p_main=3, q_main=11;
     
     printf("%d\n", achar_n(p_main,q_main));
 
-    verificar_nums_primos_p_q(p_main,q_main);
 
     return 0;
 }
