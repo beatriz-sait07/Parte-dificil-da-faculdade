8d945a3e7622d41c8519dd0e2a75ba746fac12ea
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2024-03-04 19:46:30 -0400

    iniciando lista encadeada

---
+++ automatos/automato_email.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+//--------------------------LISTA ENCADEADA----------------------
+
+struct node{
+    char letras; //criar o nome/email e afins
+    struct node *next;
+};
+
+struct lista{
+    struct node *inicio, *fim;
+};
+
+struct node *criar_no(char letra){
+    struct node *no = (struct node*)calloc(1, sizeof(struct node));
+    no->letras = letra;
+    no->next = NULL;
+
+    return no;
+}
+
+struct lista *criar_lista(){
+    struct lista *l = (struct lista*)calloc(1, sizeof(struct lista));
+    l->inicio = NULL;
+    l->fim = NULL;
+
+    return l;
+}
+
+int main (){
+
+    return 0;
+}
